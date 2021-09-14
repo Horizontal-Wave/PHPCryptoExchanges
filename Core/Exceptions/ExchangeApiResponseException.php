2f5942749e5f3a2033c219461942734ae6e69ac9
FournyP <pierre.fourny76@gmail.com>
2021-09-14 19:28:34 +0200

    Adding param to ExchangeApiResponseException

--- Core/Exceptions/ExchangeApiResponseException.php
+++ Core/Exceptions/ExchangeApiResponseException.php
@@ -4,8 +4,8 @@ namespace CryptoExchanges\Core\Exceptions;
 
 class ExchangeApiResponseException extends \Exception 
 {
-    public function __construct(int $statusCode, string $exchangeName, string $route)
+    public function __construct(int $statusCode, string $exchangeName, string $route, string $errorMessage)
     {
-        $this->message = "Error while requesting " . $exchangeName . " endpoint " . $route . ". The status code receive is : " . $statusCode;
+        $this->message = "Error while requesting " . $exchangeName . " endpoint " . $route . ". The status code receive is : " . $statusCode . ", with error message : \n" . $errorMessage;
     }
 }
