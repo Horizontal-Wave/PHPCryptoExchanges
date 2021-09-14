335a94e487a0baccd496cd21517497451b902cda
FournyP <pierre.fourny76@gmail.com>
2021-09-14 18:40:47 +0200

    Create and implement BinancePerpetualFuturesApiClient

---
+++ Binance/PerpetualFutures/BinancePerpetualFuturesApiClient.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace CryptoExchanges\Binance\Spot;
+
+use CryptoExchanges\Binance\BinanceApiClient;
+
+class BinancePerpetualFuturesApiClient extends BinanceApiClient
+{
+    protected function getRouteConfigFilePath(): string
+    {
+        return __DIR__ . "/binance_perpetual_future_api_v1.json";
+    }
+
+    protected function getOpenOrderRouteName(): string
+    {
+        return "New Order (TRADE)";
+    }
+
+    protected function getCancelOrderRouteName(): string
+    {
+        return "Cancel Order";
+    }
+
+    protected function getQueryOrderRouteName(): string
+    {
+        return "Query Order";
+    }
+
+    protected function getCurrentOrderRouteName(): string
+    {
+        return "Current All Open Orders (USER_DATA)";
+    }
+
+    protected function getAllOrderRouteName(): string
+    {
+        return "All Orders";
+    }
+
+    protected function getOrderBookRouteName(): string
+    {
+        return "Order Book (MARKET_DATA)";
+    }
+
+    protected function getCandlestickDataRouteName(): string
+    {
+        return "Kline/Candlestick Data (MARKET_DATA)";
+    }
+}
