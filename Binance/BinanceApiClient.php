8c1d4ba38d24e65a149ce12e8cae06502bc1eb55
FournyP <pierre.fourny76@gmail.com>
2021-09-16 22:27:47 +0200

    Fix BinanceApiClient and implement Exceptions

--- Binance/BinanceApiClient.php
+++ Binance/BinanceApiClient.php
@@ -2,10 +2,12 @@
 
 namespace CryptoExchanges\Binance;
 
+use Symfony\Contracts\HttpClient\ResponseInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use CryptoExchanges\Core\Utils\UrlEncoder;
+use CryptoExchanges\Core\Exceptions\WeightRateLimitException;
 use CryptoExchanges\Core\Exceptions\RouteConfigNotFoundException;
-use CryptoExchanges\Core\Exceptions\ExchangeApiResponseException;
+use CryptoExchanges\Core\Exceptions\ExchangeApiErrorResponseException;
 use CryptoExchanges\Core\EntityInterfaces\ExchangeEntityInterface;
 use CryptoExchanges\Core\EntityInterfaces\ApiKeyEntityInterface;
 use CryptoExchanges\Core\Client\ExchangeApiClient;
@@ -21,44 +23,25 @@ abstract class BinanceApiClient extends ExchangeApiClient
         $this->urlEncoder = $urlEncoder;
     }
 
+    /**
+     * Method to call the api
+     *
+     * @param string $routeName
+     * @param ApiKeyEntityInterface|null $apiKey
+     * @param array $params
+     * @return array
+     * 
+     * @throws RouteConfigNotFoundException
+     * @throws WeightRateLimitException
+     * @throws ExchangeApiErrorResponseException
+     */
     public function callApi(string $routeName, ?ApiKeyEntityInterface $apiKey, array $params = []): array
     {
         $routeConfig = $this->retrieveRoute($routeName);
-
-        if ($routeConfig == null) {
-            throw new RouteConfigNotFoundException($this->exchange->getName(), $routeName);
-        }
-
         $requestConfig = $routeConfig['request'];
 
-        $url = $this->exchange->getBaseUrl() . "/" . \join('/', $requestConfig['url']['path']);
-        $headers = $this->generateHeader($requestConfig['header'], $apiKey);
-
-        if (isset($requestConfig['url']['query'])) {
-            foreach ($requestConfig['url']['query'] as $query) {
-                if ($query['key'] == 'timestamp') {
-                    $params['timestamp'] = round(microtime(true) * 1000); //Add timestamp parameter
-                }
-
-                if ($query['key'] == 'signature') {
-                    $params['signature'] = $this->generateSign($this->urlEncoder->urlEncode($params), $apiKey);
-                }
-            }
-        }
-
-        $response = $this->client->request($requestConfig['method'], $url, [
-            'headers' => $headers,
-            'query' => $requestConfig['method'] === 'GET' ? $params : null,
-            'body' => $requestConfig['method'] === 'GET' ? null : $params,
-        ]);
-
-        $content = \json_decode($response->getContent(false), true);
-
-        if ($response->getStatusCode() >= 300 || $response->getStatusCode() < 200) {
-            throw new ExchangeApiResponseException($response->getStatusCode(), $this->exchange->getName(), $url, $content['msg']);
-        }
-
-        return $content;
+        $response = $this->buildRequest($params, $requestConfig, $apiKey);
+        return $this->handleResponse($response, $routeConfig);
     }
 
     private function generateHeader(array $requiredHeaders, ?ApiKeyEntityInterface $apiKey): array
@@ -81,6 +64,14 @@ abstract class BinanceApiClient extends ExchangeApiClient
         return \hash_hmac('SHA256', $data, $apiKey->getPrivateKey());
     }
 
+    /**
+     * Method to retrieve the route in the config file
+     *
+     * @param string $routeName
+     * @return array
+     * 
+     * @throws RouteConfigNotFoundException
+     */
     private function retrieveRoute(string $routeName): array
     {
         $routeConfigs = $this->fetchRouteConfigs();
@@ -96,6 +87,81 @@ abstract class BinanceApiClient extends ExchangeApiClient
             $index++;
         }
 
+        if ($result == null) {
+            throw new RouteConfigNotFoundException($this->exchange->getName(), $routeName);
+        }
+
         return $result;
+    }
+
+    /**
+     * Method to build the parameters
+     *
+     * @param array $params
+     * @param array $requestConfig
+     * @param ApiKeyEntityInterface|null $apiKey
+     * @return array
+     */
+    private function buildParams(array $params, array $requestConfig, ?ApiKeyEntityInterface $apiKey) : array
+    {
+        if (isset($requestConfig['url']['query'])) {
+            foreach ($requestConfig['url']['query'] as $query) {
+                if ($query['key'] == 'timestamp') {
+                    $params['timestamp'] = round(microtime(true) * 1000); //Add timestamp parameter
+                }
+
+                if ($query['key'] == 'signature') {
+                    $params['signature'] = $this->generateSign($this->urlEncoder->urlEncode($params), $apiKey);
+                }
+            }
+        }
+
+        return $params;
+    }
+
+    /**
+     * Method to build the request
+     *
+     * @param array $params
+     * @param array $requestConfig
+     * @param ApiKeyEntityInterface|null $apiKey
+     * @return ResponseInterface
+     */
+    private function buildRequest(array $params, array $requestConfig, ?ApiKeyEntityInterface $apiKey) : ResponseInterface
+    {
+        $url = $this->exchange->getBaseUrl() . "/" . \join('/', $requestConfig['url']['path']);
+        $headers = $this->generateHeader($requestConfig['header'], $apiKey);
+
+        $params = $this->buildParams($params, $requestConfig, $apiKey);
+
+        return $this->client->request($requestConfig['method'], $url, [
+            'headers' => $headers,
+            'query' => $requestConfig['method'] === 'GET' ? $params : null,
+            'body' => $requestConfig['method'] === 'GET' ? null : $params,
+        ]);
+    }
+
+    /**
+     * Method to handle the response
+     *
+     * @param ResponseInterface $response
+     * @return array
+     * 
+     * @throws WeightRateLimitException
+     * @throws ExchangeApiErrorResponseException
+     */
+    private function handleResponse(ResponseInterface $response, array $routeConfig) : array
+    {
+        $responseCode = $response->getStatusCode();
+
+        if ($responseCode >= 300 || $responseCode < 200) {
+            if ($responseCode == 429 || $responseCode == 418) {
+                throw new WeightRateLimitException($this->exchange->getName(), $routeConfig['name'], $response, "Retry-After");
+            }
+
+            throw new ExchangeApiErrorResponseException($this->exchange->getName(), $routeConfig['name'], $response);
+        }
+
+        return $response->toArray(false);
     }
 }
